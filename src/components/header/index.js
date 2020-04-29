6469e5eb7bc1c40d0e12e428e3836fc48dbe2e37
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2020-04-28 23:25:54 -0300

    adicionando ao carrinho

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-
 import {MdShoppingBasket} from 'react-icons/md';
+import {connect} from 'react-redux';
 
 import {Container, Cart} from './styles';
 
 import logo from '../../assets/images/logo.svg';
 
-export default function Header() {
-  return (
-    <Container>
-      <Link to="/">
-        <img src={logo} alt="rocketshoes" />
-      </Link>
+class Header extends Component {
+  render() {
+    const {cartSize} = this.props;
 
-      <Cart to="/cart">
-        <div>
-          <strong>Meu carrinho</strong>
-          <span>3 itens</span>
-        </div>
-        <MdShoppingBasket size={36} color="#fff" />
-      </Cart>
-    </Container>
-  );
+    return (
+      <Container>
+        <Link to="/">
+          <img src={logo} alt="rocketshoes" />
+        </Link>
+
+        <Cart to="/cart">
+          <div>
+            <strong>Meu carrinho</strong>
+            <span>{cartSize} itens</span>
+          </div>
+          <MdShoppingBasket size={36} color="#fff" />
+        </Cart>
+      </Container>
+    );
+  }
 }
+
+export default connect((state) => ({
+  cartSize: state.cart.length,
+}))(Header);
