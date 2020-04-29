4c7d22741a8437df346bb687deb5838647bf6fe6
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2020-04-29 01:37:45 -0300

    removendo produto

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -8,7 +8,7 @@ import {
 
 import {Container, ProductTable, Total} from './styles';
 
-function Cart({cart}) {
+function Cart({cart, dispatch}) {
   return (
     <Container>
       <ProductTable>
@@ -51,7 +51,10 @@ function Cart({cart}) {
               </td>
 
               <td>
-                <button>
+                <button
+                  onClick={() =>
+                    dispatch({type: 'REMOVE_FROM_CART', id: product.id})
+                  }>
                   <MdDelete size={20} color="#7159c1" />
                 </button>
               </td>
