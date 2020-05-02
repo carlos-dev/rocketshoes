09e50e01953338c22bf7ea863c883758cf7b65e5
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2020-05-02 19:14:08 -0300

    configurando reactotron com saga e separando as actions

--- src/store/module/cart/reducer.js
+++ src/store/module/cart/reducer.js
@@ -2,18 +2,11 @@ import produce from 'immer';
 
 export default function cart(state = [], action) {
   switch (action.type) {
-    case 'ADD_TO_CART':
+    case 'ADD_TO_CART_SUCCESS':
       return produce(state, (draft) => {
-        const productIndex = draft.findIndex((p) => p.id === action.product.id);
+        const {product} = action;
 
-        if (productIndex >= 0) {
-          draft[productIndex].amount += 1;
-        } else {
-          draft.push({
-            ...action.product,
-            amount: 1,
-          });
-        }
+        draft.push(product);
       });
     case 'REMOVE_FROM_CART':
       return produce(state, (draft) => {
